06fd74f63ecf0f6d6e17d21f51acd21fd2133275
Bart Lammers <bart.f.lammers@gmail.com>
2021-04-18 22:47:47 +0200

    Backtesting multi-coin, not yet thoroughly tested

--- tradingtools/workflow/analysis.py
+++ tradingtools/workflow/analysis.py
@@ -50,10 +50,10 @@ def plot_pairs(df, from_idx=0, to_idx=None):
         if "upcoming_gap" in df_pair:
             df_gaps = df_pair["price"][df_pair["upcoming_gap"]]
             df_gaps.plot(ax=axes[i + 1], marker="X", linestyle="None", color="orange")
-        
+
         # Align x-axis
         axes[i + 1].set_xlim(dates["min_date"], dates["max_date"])
-    
+
     # Align x-axis for All - standardized plot
     axes[0].set_xlim(dates["min_date"], dates["max_date"])
 
@@ -61,17 +61,16 @@ def plot_pairs(df, from_idx=0, to_idx=None):
     fig.tight_layout()
 
 
-def backtest(df, signals, cost_factor=0.001, verbose=True):
+def backtest(df, cost_factor=0.001, verbose=True):
 
-    buy, sell = signals == "buy", signals == "sell"
+    buy, sell = df["signals"] == "buy", df["signals"] == "sell"
 
     exposed = False
     buy_price = 0
     profit = 0
     n_orders = 0
 
-    profit_over_time = np.zerosa
-    (len(price))
+    profit_over_time = np.zeros(len(price))
     exposed_over_time = np.zeros(len(price))
 
     if verbose:
@@ -119,8 +118,157 @@ def objective(params, price, history, ahead, gaps, cost_factor=0.002):
     Returns:
         float: profit over provided data window
     """
-    
+
     signals = create_signals(history, ahead, args=params)
     profit, _, _ = backtest(price, signals, gaps, cost_factor, verbose=False)
-    
+
     return profit[-1]
+
+
+class Portfolio:
+    def __init__(self, starting_capital=1000, cost_factor=0.001):
+
+        # EVERYTHING EXPRESSED IN BASE CURRENCY
+        self.starting_capital = starting_capital
+        self.capital_unallocated = starting_capital
+        self.capital_allocated = 0
+        self.latest_price_asset = None
+
+        self.current_target_fraction = 0
+        self.cost_factor = cost_factor
+        self.quantity_asset = 0
+
+    def update(self, price, signal, target_fraction):
+
+        assert price > 0
+        assert signal in {"buy", "sell", "hold"}
+        assert target_fraction >= 0 and target_fraction <= 1
+
+        # Update price, capital and current fraction
+        self.latest_price_asset = price
+        self.capital_allocated = self.quantity_asset * price
+
+        if (
+            signal in ["buy", "sell"]
+            and target_fraction != self.current_target_fraction
+        ):
+
+            # Calculate fraction of total value that is allocated
+            total_value = self.get_total_value()
+            actual_fraction_allocated = self.capital_allocated / total_value
+
+            # Determine effective difference in fraction
+            fraction_diff = target_fraction - actual_fraction_allocated
+
+            # Determine value diffs
+            order_value = fraction_diff * total_value
+            abs_order_min_cost = abs(order_value) * (1 - self.cost_factor)
+
+            # Buy
+            if signal == "buy":
+                assert order_value > 0, "Buy signal with non-positive order value"
+                self.capital_unallocated -= abs(order_value)
+                self.capital_allocated += abs_order_min_cost
+                self.quantity_asset += abs_order_min_cost / price
+
+            # Sell
+            if signal == "sell":
+                assert order_value < 0, "Sell signal with non-positive order value"
+                self.capital_unallocated += abs_order_min_cost
+                self.capital_allocated -= abs(order_value)
+                self.quantity_asset -= abs(order_value) / price
+
+            self.current_target_fraction = target_fraction
+
+    def get_total_value(self):
+        return self.capital_unallocated + self.capital_allocated
+
+    def get_profit(self):
+        return (
+            (self.get_total_value() - self.starting_capital)
+            / self.starting_capital
+            * 100
+        )
+
+    def get_quantity(self):
+        return self.quantity_asset
+
+
+def backtest_pair(
+    df, starting_capital=1000, cost_factor=0.001, verbose=True, track_metrics=True
+):
+
+    portfolio = Portfolio(starting_capital, cost_factor)
+    ticks_backtest = []
+
+    ticks = enumerate(df.itertuples())
+
+    if verbose:
+        ticks = tqdm(ticks, total=len(df))
+
+    for i, tick in ticks:
+
+        if tick.upcoming_gap and portfolio.get_quantity() > 0:
+            portfolio.update(tick.price, "sell", 0)
+
+        else:
+
+            portfolio.update(tick.price, tick.signals, tick.target_fraction)
+
+            if track_metrics:
+                ticks_backtest.append(
+                    {
+                        "datetime": tick.Index,
+                        "value": portfolio.get_total_value(),
+                        "profit": portfolio.get_profit(),
+                        "quantity": portfolio.get_quantity(),
+                    }
+                )
+
+    if not track_metrics:
+        return portfolio.get_profit()
+
+    df_backtest = pd.DataFrame(ticks_backtest)
+    df_backtest = df_backtest.set_index("datetime")
+    return df_backtest
+
+
+if __name__ == "__main__":
+
+    p = Portfolio()
+    df = pd.read_pickle("./notebooks/temp.pickle")
+
+    backtest_pair(df, verbose=False)
+
+    exit()
+
+    p.update(100, "buy", 0.1)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(100, "hold", 0)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(100, "buy", 0.2)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(1000, "buy", 0.2)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(800, "hold", 0)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(800, "hold", 0)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(900, "sell", 0.1)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(950, "sell", 0.1)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(900, "sell", 0)
+    print(p.get_total_value())
+    print(p.get_profit())
+    p.update(900, "sell", 0)
+    print(p.get_total_value())
+    print(p.get_profit())
