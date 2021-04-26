d27bd72a0670d7b563df15a2233ebf5644374a2a
Bart Lammers <bart.f.lammers@gmail.com>
2021-04-26 23:37:51 +0200

    Simplified labelling logic

--- tradingtools/workflow/labelling.py
+++ tradingtools/workflow/labelling.py
@@ -7,65 +7,61 @@ import numpy as np
 def judge_buy(
     history,
     ahead,
-    b_irr=0.003,
-    b_n_irr=190,
-    b_p_irr=0.3, 
-    b_nrr=0.00025, 
-    b_n_nrr=30,
-    b_p_nrr=0.06,
-    b_incr_nh=3,
-    b_incr_na=7,
+    b_rate=0.003,
+    b_n_rate=190,
+    b_p_rate=0.3,
+    b_neg_rate_allowed=0.001,
+    b_n_incr_before=3,
+    b_n_incr_after=7,
 ):
 
     # Divide history into current price and actual history
     current, history = history[:, -1], history[:, :-1]
 
     # In the money check
-    irr_ahead = ahead[:, :b_n_irr] > (current * (1 + b_irr))[:, None]
-    irr_check = irr_ahead.mean(axis=1) > b_p_irr
+    rate_ahead = ahead[:, :b_n_rate] > (current * (1 + b_rate))[:, None]
+    rate_check = rate_ahead.mean(axis=1) > b_p_rate
 
     # Negative rate check
-    nrr_ahead = ahead[:, :b_n_nrr] < (current * (1 - b_nrr))[:, None]
-    nrr_check = nrr_ahead.mean(axis=1) < b_p_nrr
+    pos_rate_ahead = ahead[:, :b_n_rate] < (current * (1 - b_neg_rate_allowed))[:, None]
+    pos_rate_check = ~np.any(pos_rate_ahead, axis=1)
 
     # Increasing check
-    incr_history = (history[:, -b_incr_nh:] < current[:, None]).all(axis=1)
-    incr_ahead = (current[:, None] < ahead[:, :b_incr_na]).all(axis=1)
+    incr_history = (history[:, -b_n_incr_before:] < current[:, None]).all(axis=1)
+    incr_ahead = (current[:, None] < ahead[:, :b_n_incr_after]).all(axis=1)
     incr_check = incr_history & incr_ahead
 
-    return irr_check & nrr_check & incr_check
+    return rate_check & pos_rate_check & incr_check
 
 
 def judge_sell(
     history,
     ahead,
-    s_irr=0.003,
-    s_n_irr=190,
-    s_p_irr=0.3, 
-    s_prr=0.0001,
-    s_n_prr=10, 
-    s_p_prr=0.15,
-    s_decr_nh=3,
-    s_decr_na=7
+    s_rate=0.003,
+    s_n_rate=190,
+    s_p_rate=0.3,
+    s_pos_rate_allowed=0.001,
+    s_n_decr_before=3,
+    s_n_decr_after=7,
 ):
 
     # Divide history into current price and actual history
     current, history = history[:, -1], history[:, :-1]
 
     # In the money check
-    irr_ahead = ahead[:, :s_n_irr] < (current * (1 - s_irr))[:, None]
-    irr_check = irr_ahead.mean(axis=1) > s_p_irr
+    rate_ahead = ahead[:, :s_n_rate] < (current * (1 - s_rate))[:, None]
+    rate_check = rate_ahead.mean(axis=1) > s_p_rate
 
     # Positive rate check
-    prr_ahead = ahead[:, :s_n_prr] > (current * (1 + s_prr))[:, None]
-    prr_check = prr_ahead.mean(axis=1) < s_p_prr
+    pos_rate_ahead = ahead[:, :s_n_rate] > (current * (1 + s_pos_rate_allowed))[:, None]
+    pos_rate_check = ~np.any(pos_rate_ahead, axis=1)
 
     # Decreasing check
-    decr_history = (history[:, -s_decr_nh:] > current[:, None]).all(axis=1)
-    decr_ahead = (current[:, None] > ahead[:, :s_decr_na]).all(axis=1)
-    decr_check = decr_history & decr_ahead
+    decr_before = (history[:, -s_n_decr_before:] > current[:, None]).all(axis=1)
+    decr_after = (current[:, None] > ahead[:, :s_n_decr_after]).all(axis=1)
+    decr_check = decr_before & decr_after
 
-    return irr_check & prr_check & decr_check
+    return rate_check & pos_rate_check & decr_check
 
 
 def _add_kwargs(fn, kwargs=None):
@@ -77,10 +73,10 @@ def _add_kwargs(fn, kwargs=None):
     # so let's filter out the ones that apply to fn
     fn_args = list(inspect.signature(fn).parameters.keys())
     kwargs = {k: v for k, v in kwargs.items() if k in fn_args}
-    
+
     def fn_wrapper(history, ahead):
         return fn(history, ahead, **kwargs)
-        
+
     return fn_wrapper
 
 
@@ -97,6 +93,3 @@ def create_signals(history, ahead, args=None):
     targets[sell] = "sell"
 
     return targets
-
-
-
